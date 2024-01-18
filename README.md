338c3e460c8777902a8c69d83d003b0f1569e82d
Brittney Coble <bwilcutt#gmail.com>
2024-01-17 22:24:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+python clipboard app from Tech with Tim. 
+good python practice reading and writing
+plus json practice
+
+practicing unit testing and test automation.
---
+++ app/multiclipboard.py
@@ -0,0 +1,26 @@
+import sys
+import clipboard
+import json
+
+
+def save_items(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+
+save_items("test.json", {"key": "value"})
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    print(command)
+
+    if command == "save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("Unknown command")
+else:
+    print("Please print exactly one command.")
